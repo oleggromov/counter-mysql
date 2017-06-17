f5d7cb46e002dd08127f1862776876b19001162f
Oleg Gromov <mail@oleggromov.com>
2017-06-16 23:52:17 -0700

    Updates init script to check password variable existence

--- init-scripts/init.sh
+++ init-scripts/init.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
+	echo "$MYSQL_ROOT_PASSWORD is unset!"
+	exit 1
+fi
+
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "ALTER DATABASE $MYSQL_DATABASE DEFAULT CHARSET 'utf8' DEFAULT COLLATE 'utf8_general_ci';"
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" $MYSQL_DATABASE < /docker-entrypoint-initdb.d/init.sql.hidden
